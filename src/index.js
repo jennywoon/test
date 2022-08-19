e925eb88dcd1d99cbf2f9d5f57b085d7ac690586
jennywoon <cjaeks0394@naver.com>
2022-08-19 23:35:23 +0900

    index.js provider 주석처리

--- src/index.js
+++ src/index.js
@@ -2,16 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import { store } from './store/config';
+// import { store } from './store/config';
 import { store } from "./redux/config/configStore";
-import { Provider } from "react-redux";
+// import { Provider } from "react-redux";
 
-// import { CookiesProvider } from "react-cookie";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  // <CookiesProvider>
-    <Provider store={store}>
+    // <Provider store={store}>
       <App />
-    </Provider>
-  // </CookiesProvider>
+    // </Provider>
 );
